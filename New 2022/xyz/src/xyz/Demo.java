a77575e4fa31e45060b15472212358d6406b0d00
Ghulam <rajm80876@gmail.com>
2023-06-14 13:22:45 +0530

    xyz changes and commit comment

---
+++ New 2022/xyz/src/xyz/Demo.java
@@ -0,0 +1,31 @@
+package xyz;
+
+public class Demo {
+	 static int k=20;
+	
+	public static void m1() {
+		int i=10;
+		System.out.println(""+i);
+		System.out.println(""+k);
+	}
+	
+
+	public static void main(String [] args) {
+		// inside method method we can write statement
+		// can we write statement outside :No it will give error 
+		// semicolon ; 
+		System.out.println("this is print ----");
+		System.out.println(5);
+		System.out.print(5);
+		System.out.println();
+		//Sysout>ctr+spacebar+enter
+		
+		System.out.println(k);
+//		int a=50;
+//		int b= 40;
+		int a =50 , b=40;
+		//syso
+		m1();
+		
+	}
+}
