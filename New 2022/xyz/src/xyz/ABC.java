62efac04d1fd65274fb4eb9202fb20b33dec5d51
Ghulam <rajm80876@gmail.com>
2023-06-15 10:33:41 +0530

    second time adding on ABC folder as implicitly wait

--- New 2022/xyz/src/xyz/ABC.java
+++ New 2022/xyz/src/xyz/ABC.java
@@ -1,5 +1,7 @@
 package xyz;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,6 +11,7 @@ public class ABC {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com");
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
 
 	}
